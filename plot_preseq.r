a7b36e4c8195f243d359352ef642f56df3420efe
keyfm <33058898+keyfm@users.noreply.github.com>
2017-11-07 22:47:18 +0100

    Update plot_preseq.r

--- plot_preseq.r
+++ plot_preseq.r
@@ -33,7 +33,7 @@ d2=d1[d1[,1]<=maxR,]
 ## plot
 maxY <- max(d2[,'UPPER_0.95CI'])+(0.1*max(d2[,'UPPER_0.95CI']))
 pdf(paste(outfolder,sampleID,"_preseq.pdf",sep=""))
-plot("",ylim=c(0,maxY),xlim=c(0,maxR),xlab="Sequencing effort",ylab="Expected distinct reads (w/ 95CI)",main=sampleID,cex.main=0.95)
+plot("",ylim=c(0,maxY),xlim=c(0,maxR),xlab="Mapped reads prior RMDup",ylab="Mapped Reads after RMDup (w/ 95CI)",main=sampleID,cex.main=0.95)
 mtext(paste("% Endo:",endo.per))
 polygon(c( d2[,'TOTAL_READS'] , rev(d2[,'TOTAL_READS']) ) , c( d2[,'LOWER_0.95CI'] , rev(d2[,'UPPER_0.95CI']) ) ,col="lightblue1",border="lightskyblue4",lwd=1.5)
 grid(NULL,NULL,lty=2,col="grey")
